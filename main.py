8d14edecfce22dc8a5b10ed5191ae35f13084748
Sorin Mihai Toma <toma.mihai.sorin23@gmail.com>
2020-09-05 00:01:25 +0300

    finished the html/css porjects and the first javascript one, two more to go

--- main.py
+++ main.py
@@ -31,12 +31,37 @@ def front_projects():
 
 @app.route('/javascript')
 def javascript_projects():
-    return "This are my Javascript projects"
+    return render_template('javascript_projects.html')
 
 
 @app.route('/full_stack')
 def full_stack_projects():
     return "This are my Full stack projects"
+
+
+@app.route('/landing_page_project')
+def landing_page_project():
+    return render_template('/html_projects/landing_page_project.html')
+
+
+@app.route('/survey_project')
+def survey_project():
+    return render_template('/html_projects/survey_project.html')
+
+
+@app.route('/tds_project')
+def tds_project():
+    return render_template('/html_projects/tds_project.html')
+
+
+@app.route('/tribute_page')
+def tribute_page():
+    return render_template('/html_projects/tribute_page.html')
+
+
+@app.route('/rock_paper_scissors')
+def rock_paper_scissors():
+    return render_template('/javascript_projects/rock_paper_scissors.html')
 
 
 def main():
